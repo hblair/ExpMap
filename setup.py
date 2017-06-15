8dffa23d7192180c3c1eb48c523450b0cbd27e0f
hblair <hblair@mymail.mines.edu>
2017-06-15 15:22:16 -0700

    Adding Files

---
+++ setup.py
@@ -0,0 +1,9 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = "ExpDat",
+      version = "0.1",
+      packages = [""],
+      package_dir = {"
